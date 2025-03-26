d406c907cf139f17d8d383ae3136de079707538b
Zoe Maas <zmaas@4sure.tech>
2025-03-26 17:28:59 +0100

    chore: Added SHOWCASE-155 changes

--- apps/bc-wallet-api-server/src/database/migrations/0006_bc-wallet-api.sql
+++ apps/bc-wallet-api-server/src/database/migrations/0006_bc-wallet-api.sql
@@ -1,4 +1,6 @@
 ALTER TABLE "credentialDefinition" ALTER COLUMN "icon" DROP NOT NULL;--> statement-breakpoint
 ALTER TABLE "stepAction" ADD COLUMN "credential_definition_id" text;--> statement-breakpoint
 ALTER TABLE "stepAction" ADD COLUMN "connection_id" text;--> statement-breakpoint
-ALTER TABLE "stepAction" ADD COLUMN "go_to_step" text;
+ALTER TABLE "stepAction" ADD COLUMN "go_to_step" text;--> statement-breakpoint
+ALTER TABLE "step" ADD COLUMN "credential_definition_id" uuid;--> statement-breakpoint
+ALTER TABLE "step" ADD CONSTRAINT "step_credential_definition_id_credentialDefinition_id_fk" FOREIGN KEY ("credential_definition_id") REFERENCES "public"."credentialDefinition"("id") ON DELETE no action ON UPDATE no action;
