1f9f6c8026a2f114b66bdaadfad672866d21c737
Zoe Maas <zmaas@4sure.tech>
2025-03-28 15:45:22 +0100

    chore: Created additional migration file

--- apps/bc-wallet-api-server/src/database/migrations/0006_bc-wallet-api.sql
+++ apps/bc-wallet-api-server/src/database/migrations/0006_bc-wallet-api.sql
@@ -1,6 +1,4 @@
 ALTER TABLE "credentialDefinition" ALTER COLUMN "icon" DROP NOT NULL;--> statement-breakpoint
 ALTER TABLE "stepAction" ADD COLUMN "credential_definition_id" text;--> statement-breakpoint
 ALTER TABLE "stepAction" ADD COLUMN "connection_id" text;--> statement-breakpoint
-ALTER TABLE "stepAction" ADD COLUMN "go_to_step" text;--> statement-breakpoint
-ALTER TABLE "step" ADD COLUMN "credential_definition_id" uuid;--> statement-breakpoint
-ALTER TABLE "step" ADD CONSTRAINT "step_credential_definition_id_credentialDefinition_id_fk" FOREIGN KEY ("credential_definition_id") REFERENCES "public"."credentialDefinition"("id") ON DELETE no action ON UPDATE no action;
+ALTER TABLE "stepAction" ADD COLUMN "go_to_step" text;
