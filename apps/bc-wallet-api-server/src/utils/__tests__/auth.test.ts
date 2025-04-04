b9fe3e05652ec2264dbab7529e437b11ffdd0779
Zoe Maas <zmaas@4sure.tech>
2025-04-04 17:00:23 +0200

    chore: Refactored auth tests

--- apps/bc-wallet-api-server/src/utils/__tests__/auth.test.ts
+++ apps/bc-wallet-api-server/src/utils/__tests__/auth.test.ts
@@ -1,128 +1,78 @@
-import { authorizationChecker } from '../auth'
-import { Action } from 'routing-controllers'
-
-jest.mock('keycloak-connect')
+import { authorizationChecker, checkRoles } from '../auth'
 
 describe('auth tests', () => {
-
-  const authorizedToken = {
-    "exp": 1743772550,
-    "iat": 1743772250,
-    "auth_time": 1743772249,
-    "jti": "7302955c-b738-4985-9cfc-0f122389e665",
-    "iss": "http://localhost:8080/realms/BC",
-    "aud": "account",
-    "sub": "07212d51-94ff-4e03-a8bc-40c41fa06016",
-    "typ": "Bearer",
-    "azp": "showcase-tenantA",
-    "sid": "b62014f6-aef7-491f-9d49-eb21c0609371",
-    "acr": "1",
-    "allowed-origins": [
-      "http://*"
-    ],
-    "realm_access": {
-      "roles": [
-        "SHOWCASE_SUPERADMIN",
-        "offline_access",
-        "uma_authorization",
-        "default-roles-bc"
-      ]
+  const authorizedToken: any = {
+    exp: 1743772550,
+    iat: 1743772250,
+    auth_time: 1743772249,
+    jti: '7302955c-b738-4985-9cfc-0f122389e665',
+    iss: 'http://localhost:8080/realms/BC',
+    aud: 'account',
+    sub: '07212d51-94ff-4e03-a8bc-40c41fa06016',
+    typ: 'Bearer',
+    azp: 'showcase-tenantA',
+    sid: 'b62014f6-aef7-491f-9d49-eb21c0609371',
+    acr: '1',
+    'allowed-origins': ['http://*'],
+    realm_access: {
+      roles: ['SHOWCASE_SUPERADMIN', 'offline_access', 'uma_authorization', 'default-roles-bc'],
     },
-    "resource_access": {
-      "showcase-tenantA": {
-        "roles": [
-          "SHOWCASE_ADMIN",
-          "SHOWCASE_PUBLISHER",
-          "SHOWCASE_EDITOR"
-        ]
+    resource_access: {
+      'showcase-tenantA': {
+        roles: ['SHOWCASE_ADMIN', 'SHOWCASE_PUBLISHER', 'SHOWCASE_EDITOR'],
       },
-      "account": {
-        "roles": [
-          "manage-account",
-          "manage-account-links",
-          "view-profile"
-        ]
-      }
+      account: {
+        roles: ['manage-account', 'manage-account-links', 'view-profile'],
+      },
     },
-    "scope": "profile email",
-    "email_verified": false,
-    "name": "Johnny B. Goode",
-    "preferred_username": "jbgoode",
-    "given_name": "Johnny B.",
-    "family_name": "Goode",
-    "email": "jbgoode@testmail.com"
-  }
-
-  // --- Define the mock function OUTSIDE the jest.mock call ---
-// This makes it easy to reference and configure in your tests.
-  const mockValidateAccessToken = jest.fn().mockResolvedValue(true);
-  const mockValidateToken = jest.fn().mockResolvedValue(authorizedToken)
-
-// --- Mock the 'keycloak-connect' module ---
-  jest.mock('keycloak-connect', () => {
-    return jest.fn().mockImplementation(() => {
-      console.log('Mock KeycloakConnect constructor called'); // For debugging
-      return {
-        // Simulate the grantManager property
-        grantManager: {
-          // Assign our pre-defined mock function to validateAccessToken
-          validateAccessToken: mockValidateAccessToken,
-          // Add mocks for other grantManager methods if your code uses them
-          validateToken: mockValidateToken
-        }
-      };
-    });
-  });
-
-  const authorizedAction: Action = {
-    request: {
-      headers: {
-        authorization: authorizedToken
-      }
-    },
-    response: {}
+    scope: 'profile email',
+    email_verified: false,
+    name: 'Johnny B. Goode',
+    preferred_username: 'jbgoode',
+    given_name: 'Johnny B.',
+    family_name: 'Goode',
+    email: 'jbgoode@testmail.com',
   }
 
   const clientId = 'showcase-tenantA'
 
-  it('should authorize user with SHOWCASE_ADMIN role', async () => {
-
+  it('should authorize user with SHOWCASE_ADMIN role', () => {
     const roles = [`${clientId}:SHOWCASE_ADMIN`]
-    await expect(authorizationChecker(authorizedAction, roles)).resolves.toBeTruthy()
+    expect(checkRoles(authorizedToken, roles)).toBeTruthy()
   })
 
-  it('should authorize user with SHOWCASE_EDITOR role', async () => {
+  it('should authorize user with SHOWCASE_EDITOR role', () => {
     const roles = [`${clientId}:SHOWCASE_EDITOR`]
-    await expect(authorizationChecker(authorizedAction, roles)).resolves.toBeTruthy()
+    expect(checkRoles(authorizedToken, roles)).toBeTruthy()
   })
 
-  it('should authorize user with SHOWCASE_PUBLISHER role', async () => {
+  it('should authorize user with SHOWCASE_PUBLISHER role', () => {
     const roles = [`${clientId}:SHOWCASE_PUBLISHER`]
-    await expect(authorizationChecker(authorizedAction, roles)).resolves.toBeTruthy()
+    expect(checkRoles(authorizedToken, roles)).toBeTruthy()
   })
 
-  it('should authorize user with SHOWCASE_SUPERADMIN role', async () => {
+  it('should authorize user with SHOWCASE_SUPERADMIN role', () => {
     const roles = ['realm:SHOWCASE_SUPERADMIN']
-    await expect(authorizationChecker(authorizedAction, roles)).resolves.toBeTruthy()
+    expect(checkRoles(authorizedToken, roles)).toBeTruthy()
   })
 
-  it.skip('should not authorize user with SHOWCASE_RANDOM role', async () => {
+  it.skip('should not authorize user with SHOWCASE_RANDOM role', () => {
     const roles = [
       'realm:SHOWCASE_SUPERADMIN',
       'showcase-tenantA:SHOWCASE_ADMIN',
       'showcase-tenantA:SHOWCASE_EDITOR',
-      'showcase-tenantA:SHOWCASE_PUBLISHER'
+      'showcase-tenantA:SHOWCASE_PUBLISHER',
     ]
-    await expect(authorizationChecker(authorizedAction, roles)).resolves.toBeFalsy()
+    expect(authorizationChecker(authorizedToken, roles)).toBeFalsy()
   })
 
-  it.skip('should not authorize user with SHOWCASE_RANDOM role', async () => {
+  it.skip('should not authorize user with SHOWCASE_RANDOM role', () => {
     const roles = [
       'realm:SHOWCASE_SUPERADMIN',
       'showcase-tenantA:SHOWCASE_ADMIN',
       'showcase-tenantA:SHOWCASE_EDITOR',
-      'showcase-tenantA:SHOWCASE_PUBLISHER'
+      'showcase-tenantA:SHOWCASE_PUBLISHER',
     ]
-    await expect(authorizationChecker(authorizedAction, roles)).resolves.toBeFalsy()
+    expect(checkRoles(authorizedToken, roles)).toBeFalsy()
   })
 })
--- apps/bc-wallet-api-server/src/utils/auth.ts
+++ apps/bc-wallet-api-server/src/utils/auth.ts
@@ -13,60 +13,6 @@ export async function authorizationChecker(action: Action, roles: string[]): Pro
   // Validates the claims and the signature of the token
   const validToken = await keycloak.grantManager.validateToken(decodedToken)
 
-  // const decodedToken = {
-  //   protectedHeader: {
-  //     "alg": "RS256",
-  //     "typ": "JWT",
-  //     "kid": "rUG2ogSG3gv51N-Z1X_gpDyGh7jkwSNw4zdwJMjPvjM"
-  //   },
-  //   payload: {
-  //     "exp": 1743709067,
-  //     "iat": 1743708767,
-  //     "auth_time": 1743708766,
-  //     "jti": "7b78e574-b8ab-45b5-91cd-b2c5353c2889",
-  //     "iss": "http://localhost:8080/realms/BC",
-  //     "aud": "account",
-  //     "sub": "464610a7-c87a-467f-9292-f9d4f28f45a4",
-  //     "typ": "Bearer",
-  //     "azp": "showcase-tenantA",
-  //     "sid": "e934dc63-ad1f-4567-a15d-efe57eb3e9c2",
-  //     "acr": "1",
-  //     "allowed-origins": [
-  //       "http://*"
-  //     ],
-  //     "realm_access": {
-  //       "roles": [
-  //         "SHOWCASE_SUPERADMIN",
-  //         "offline_access",
-  //         "uma_authorization",
-  //         "default-roles-bc"
-  //       ]
-  //     },
-  //     "resource_access": {
-  //       "showcase-tenantA": {
-  //         "roles": [
-  //           "SHOWCASE_ADMIN",
-  //           "SHOWCASE_PUBLISHER",
-  //           "SHOWCASE_EDITOR"
-  //         ]
-  //       },
-  //       "account": {
-  //         "roles": [
-  //           "manage-account",
-  //           "manage-account-links",
-  //           "view-profile"
-  //         ]
-  //       }
-  //     },
-  //     "scope": "profile email",
-  //     "email_verified": false,
-  //     "name": "Zoe Maas",
-  //     "preferred_username": "zoe",
-  //     "given_name": "Zoe",
-  //     "family_name": "Maas",
-  //     "email": "zmaas@4sure.tech"
-  //   }
-  // }
   return checkRoles(validToken, roles)
 }
 
