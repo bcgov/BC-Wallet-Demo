99713294f975603a97ba61931a3971921a58b911
Ronaldo Lima <ronal2do@gmail.com>
2025-04-07 23:47:44 +0200

    Refactor Keycloak authentication to utilize centralized environment variable management. Updated auth.ts to import environment variables from the new env module for improved consistency and maintainability.

--- apps/bc-wallet-showcase-creator/auth.ts
+++ apps/bc-wallet-showcase-creator/auth.ts
@@ -1,19 +1,12 @@
 import NextAuth from "next-auth"
 import Keycloak from "next-auth/providers/keycloak"
-
-const AUTH_KEYCLOAK_ID = process.env.AUTH_KEYCLOAK_ID
-const AUTH_KEYCLOAK_SECRET = process.env.AUTH_KEYCLOAK_SECRET
-const AUTH_KEYCLOAK_ISSUER = process.env.AUTH_KEYCLOAK_ISSUER
-
-if (!AUTH_KEYCLOAK_ID || !AUTH_KEYCLOAK_SECRET || !AUTH_KEYCLOAK_ISSUER) {
-  throw new Error("Missing Keycloak credentials")
-}
+import { env } from "@/env"
  
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [Keycloak({
-    clientId: process.env.AUTH_KEYCLOAK_ID!,
-    clientSecret: process.env.AUTH_KEYCLOAK_SECRET!,
-    issuer: process.env.AUTH_KEYCLOAK_ISSUER!,
+    clientId: env.AUTH_KEYCLOAK_ID!,
+    clientSecret: env.AUTH_KEYCLOAK_SECRET!,
+    issuer: env.AUTH_KEYCLOAK_ISSUER!,
   })],
   callbacks: {
     jwt: async ({ token, user, account }) => {
