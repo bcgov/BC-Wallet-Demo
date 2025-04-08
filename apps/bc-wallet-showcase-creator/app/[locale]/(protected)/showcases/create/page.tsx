95c5d6a2fff9581cfdb32821c31d0f9f23698e8a
Ronaldo Lima <ronal2do@gmail.com>
2025-04-08 13:11:33 +0200

    Enhance Create Onboarding Page with step-by-step guide for showcase creation. Updated headings and added detailed instructions to improve user experience.

--- apps/bc-wallet-showcase-creator/app/[locale]/(protected)/showcases/create/page.tsx
+++ apps/bc-wallet-showcase-creator/app/[locale]/(protected)/showcases/create/page.tsx
@@ -21,10 +21,27 @@ export default function CreateOnboardingPage() {
         <div className="flex gap-4 p-4 h-fit-content">
           <div className="w-1/3 bg-[white] dark:bg-dark-bg-secondary border shadow-md rounded-md flex flex-col">
             <div className="p-4 border-b shadow">
-              <h2 className="text-base font-bold text-foreground">{t('showcases.publish_info_title')}</h2>
-              <p className="w-full text-xs text-foreground/80">{t('showcases.publish_info_subtitle')}</p>
+              <h2 className="text-base font-bold text-foreground">How to create a showcase</h2>
+              <p className="w-full text-xs text-foreground/80">
+                This is a step by step guide to help you create a showcase.
+              </p>
             </div>
-            {/* <PublishInfo characters={personasToDisplay as Partial<Persona>[]} credentials={displayShowcase.credentialDefinitions} /> */}
+            <div className="p-4">
+              <ul className="flex flex-col gap-4">
+                <li className="flex flex-col gap-2">
+                  <h3 className="text-sm font-bold">Step 1: Create a showcase</h3>
+                  <p className="text-xs text-foreground/80">This is the first step to create a showcase.</p>
+                </li>
+                <li className="flex flex-col gap-2">
+                  <h3 className="text-sm font-bold">Step 2: Create a showcase</h3>
+                  <p className="text-xs text-foreground/80">This is the first step to create a showcase.</p>
+                </li>
+                <li className="flex flex-col gap-2">
+                  <h3 className="text-sm font-bold">Step 3: Create a showcase</h3>
+                  <p className="text-xs text-foreground/80">This is the first step to create a showcase.</p>
+                </li>
+              </ul>
+            </div>
           </div>
           <div className="w-2/3 bg-white dark:bg-dark-bg-secondary border shadow-md rounded-md flex flex-col">
             <ShowcaseCreate />
