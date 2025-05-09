19fc6790f54ae5e133424a00e8e6e5da7e37b461
sanderPostma <spostma@sphereon.com>
2025-05-09 18:01:14 +0200

    chore: fixed tests

--- apps/bc-wallet-api-server/src/database/repositories/__tests__/showcase.repository.test.ts
+++ apps/bc-wallet-api-server/src/database/repositories/__tests__/showcase.repository.test.ts
@@ -26,6 +26,7 @@ import {
   StepActionType,
   StepType,
   Tenant,
+  TenantType,
   User,
 } from '../../../types'
 import * as schema from '../../schema'
@@ -224,6 +225,7 @@ describe('Database showcase repository tests', (): void => {
     const tenantRepository = Container.get(TenantRepository)
     const newTenant: NewTenant = {
       id: '79a56be5-89bd-40dc-a6a7-fc035487e437',
+      tenantType: TenantType.SHOWCASE,
       realm: 'test_realm',
       clientId: 'test_client_id',
       clientSecret: 'super_secret',
--- apps/bc-wallet-api-server/src/database/repositories/__tests__/user.repository.test.ts
+++ apps/bc-wallet-api-server/src/database/repositories/__tests__/user.repository.test.ts
@@ -6,7 +6,7 @@ import { drizzle } from 'drizzle-orm/pglite'
 import { Container } from 'typedi'
 
 import DatabaseService from '../../../services/DatabaseService'
-import { NewTenant, NewUser } from '../../../types'
+import { NewTenant, NewUser, TenantType } from '../../../types'
 import * as schema from '../../schema'
 import TenantRepository from '../TenantRepository'
 import UserRepository from '../UserRepository'
@@ -123,10 +123,11 @@ describe('Database user repository tests', (): void => {
 
   it('Should save user with tenants to database', async (): Promise<void> => {
     const tenant: NewTenant = {
+      tenantType: TenantType.SHOWCASE,
       id: 'test-tenant-id',
-      realm: 'test_realm',
-      clientId: 'test_client_id',
-      clientSecret: 'super_secret',
+      oidcRealm: 'test_realm',
+      oidcClientId: 'test_client_id',
+      oidcClientSecret: 'super_secret',
     }
     const savedTenant = await tenantRepository.create(tenant)
     expect(savedTenant).toBeDefined()
@@ -156,15 +157,17 @@ describe('Database user repository tests', (): void => {
   it('Should update user with tenants in database', async (): Promise<void> => {
     const tenant1: NewTenant = {
       id: 'test-tenant-id-1',
-      realm: 'test_realm',
-      clientId: 'test_client_id',
-      clientSecret: 'super_secret',
+      tenantType: TenantType.SHOWCASE,
+      oidcRealm: 'test_realm',
+      oidcClientId: 'test_client_id',
+      oidcClientSecret: 'super_secret',
     }
     const tenant2: NewTenant = {
       id: 'test-tenant-id-2',
-      realm: 'test_realm',
-      clientId: 'test_client_id',
-      clientSecret: 'super_secret',
+      tenantType: TenantType.SHOWCASE,
+      oidcRealm: 'test_realm',
+      oidcClientId: 'test_client_id',
+      oidcClientSecret: 'super_secret',
     }
 
     const savedTenant1 = await tenantRepository.create(tenant1)
@@ -183,13 +186,17 @@ describe('Database user repository tests', (): void => {
     expect(savedUser.tenants).toHaveLength(1)
     expect(savedUser.tenants).toEqual([
       {
-        clientId: 'test_client_id',
-        clientSecret: 'super_secret',
         createdAt: expect.any(Date),
         deletedAt: null,
         id: 'test-tenant-id-1',
         nonceBase64: null,
-        realm: 'test_realm',
+        oidcClientId: 'test_client_id',
+        oidcClientSecret: 'super_secret',
+        oidcRealm: 'test_realm',
+        tenantType: 'SHOWCASE',
+        tractionApiUrl: null,
+        tractionTenantId: null,
+        tractionWalletId: null,
         updatedAt: expect.any(Date),
       },
     ])
