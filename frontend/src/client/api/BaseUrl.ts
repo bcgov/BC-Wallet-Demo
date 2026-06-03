a745790d35cdeb28e3ef51351e4c2bd055b1da30
jamshale <jamiehalebc@gmail.com>
2026-06-03 11:48:57 -0700

    Grammer msitake
    Signed-off-by: jamshale <jamiehalebc@gmail.com>

--- frontend/src/client/api/BaseUrl.ts
+++ frontend/src/client/api/BaseUrl.ts
@@ -9,7 +9,7 @@ export const baseUrl = (import.meta.env.VITE_HOST_BACKEND || '') + baseRoute
 export const baseWsUrl = import.meta.env.VITE_HOST_BACKEND || ''
 export const socketPath = `${baseRoute}/demo/socket/`
 
-// This warning is a false flag. THis is the way we want to import this.
+// This warning is a false flag. This is the way we want to import this.
 // eslint-disable-next-line import/no-named-as-default-member
 export const apiCall = axios.create({ baseURL: baseUrl })
 
