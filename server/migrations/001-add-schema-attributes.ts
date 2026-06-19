b17a43d759805fb8b9dcc9e1196d11907236974e
jamshale <jamiehalebc@gmail.com>
2026-06-19 11:33:16 -0700

    Add migration error handling/logging
    Signed-off-by: jamshale <jamiehalebc@gmail.com>

--- server/migrations/001-add-schema-attributes.ts
+++ server/migrations/001-add-schema-attributes.ts
@@ -1,39 +1,51 @@
 import { SchemaModel } from '../src/db/models/Schema'
+import logger from '../src/utils/logger'
 
 import schemaTypeSeedValues from './values/attributeTypes.json'
 
 export async function up() {
-  await SchemaModel.updateMany(
-    { attrNames: { $exists: true } },
-    [
-      {
-        $set: {
-          attributes: {
-            $map: {
-              input: '$attrNames',
-              as: 'name',
-              in: {
-                name: '$$name',
-                type: 'string',
+  try {
+    logger.info('Migration 001: Starting schema attribute transformation')
+
+    const updateResult = await SchemaModel.updateMany(
+      { attrNames: { $exists: true } },
+      [
+        {
+          $set: {
+            attributes: {
+              $map: {
+                input: '$attrNames',
+                as: 'name',
+                in: {
+                  name: '$$name',
+                  type: 'string',
+                },
               },
             },
           },
         },
-      },
-    ],
-    { updatePipeline: true },
-  )
-  // Update the seed schemas with the correct attribute types. All other schemas will be left with the default 'string' type for all attributes.
-  await Promise.all(
-    schemaTypeSeedValues.map((schema: any) =>
-      SchemaModel.updateOne(
-        { name: schema.name, version: schema.version },
-        {
-          $set: {
-            attributes: schema.attributes,
+      ],
+      { updatePipeline: true },
+    )
+    logger.info({ modifiedCount: updateResult.modifiedCount }, 'Migration 001: Generic schema attributes updated')
+
+    // Update the seed schemas with the correct attribute types. All other schemas will be left with the default 'string' type for all attributes.
+    const seedUpdateResults = await Promise.all(
+      schemaTypeSeedValues.map((schema: any) =>
+        SchemaModel.updateOne(
+          { name: schema.name, version: schema.version },
+          {
+            $set: {
+              attributes: schema.attributes,
+            },
           },
-        },
+        ),
       ),
-    ),
-  )
+    )
+    logger.info({ count: seedUpdateResults.length }, 'Migration 001: Seed schema attributes updated')
+    logger.info('Migration 001 completed')
+  } catch (error) {
+    logger.error({ error }, 'Error in migration 001')
+    throw error
+  }
 }
